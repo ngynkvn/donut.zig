f6a901d247e50d68b4adb8348501ed970cfbfd08
Kevin <ngynkvn@gmail.com>
2024-10-13 03:28:12 -0400

    feat: Add tracy

--- build.zig
+++ build.zig
@@ -12,6 +12,31 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    // Options
+    const exe_options = b.addOptions();
+    const tracy = b.option([]const u8, "tracy", "Enable Tracy integration. Supply path to Tracy source");
+    const tracy_callstack = b.option(bool, "tracy-callstack", "Include callstack information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
+    const tracy_allocation = b.option(bool, "tracy-allocation", "Include allocation information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
+    exe_options.addOption(bool, "enable_tracy", tracy != null);
+    exe_options.addOption(bool, "enable_tracy_callstack", tracy_callstack);
+    exe_options.addOption(bool, "enable_tracy_allocation", tracy_allocation);
+    if (tracy) |tracy_path| {
+        const client_cpp = b.pathJoin(
+            &[_][]const u8{ tracy_path, "public", "TracyClient.cpp" },
+        );
+
+        exe.addIncludePath(.{ .cwd_relative = tracy_path });
+        exe.addCSourceFile(.{ .file = .{ .cwd_relative = client_cpp }, .flags = &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" } });
+        exe.root_module.linkSystemLibrary("c++", .{ .use_pkg_config = .no });
+        exe.linkLibC();
+
+        if (target.result.os.tag == .windows) {
+            exe.linkSystemLibrary("dbghelp");
+            exe.linkSystemLibrary("ws2_32");
+        }
+    }
+    exe.root_module.addOptions("build_options", exe_options);
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
